ab102009460e9564c8500ccdb6cb5c161e08feb2
AungChnaPyae <138773319+aungchanpyae33@users.noreply.github.com>
2024-02-06 22:41:18 +0630

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
-  <title>Document</title>
-</head>
-<body>
-  <h1>Hello</h1>
-</body>
-</html>
+<?php
+  $directory = "../photo";
+  // scandir is to use to get the folder like an array
+  // array_diff(make rmeove some)
+  $filesAndFolders = array_diff(scandir($directory , SCANDIR_SORT_NONE), array('.', '..'));
+   //make filepath array that filemtime can access
+  $file =  array_map( function($title) use ($directory){
+    return $directory . '/' .$title;
+  } , $filesAndFolders);
+
+  usort($file, function($a,$b){return filemtime($a)-filemtime($b);});
+
+  // step- remove "../" "replace
+
+  $fileJavascript = json_encode($file);
+  print_r($fileJavascript);
